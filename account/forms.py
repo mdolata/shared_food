1c0830bcb026fe8b79bb52cb337516edf5f43486
mdolata <mateuszdolata2@gmail.com>
2017-11-12 01:27:30 +0100

    remove unused form

--- account/forms.py
+++ account/forms.py
@@ -2,11 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
-
 class UserRegistrationForm(forms.ModelForm):
 
     password1 = forms.CharField(label="haslo", widget=forms.PasswordInput)
